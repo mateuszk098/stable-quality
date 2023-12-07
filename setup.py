89cfb3a950aea0806809690ca16a218cf712558f
Mateusz Kowalczyk <mkowalczyk013@gmail.com>
2023-12-07 19:54:09 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Resilient Tyre Quality Vision is a project that utilizes residual neural networks to solve the image classification problem - initially used to classify tyres as defective / non-defective.',
+    author='Mateusz Kowalczyk',
+    license='MIT',
+)
